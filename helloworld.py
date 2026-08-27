ba48abd2848da8ea4edfb49b66dae5a7df8e762f
SS3xyBEAST214 <ewilhit@hotmail.com>
2026-08-27 22:58:13 +0000

    wewew

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+import numpy
+
+Happyfeet=[1,3,5,7,9]
+
+a=numpy.array(Happyfeet)
+
+print(Happyfeet)
+
+numbers=[1,2,3,4,5]
+numbers2=[6,7,8,9,10]
+numbers3=[[1,2,3,4,5],[6,7,8,9,10]]
+
+print(numbers3)
+print("hello world")
+
