e3950bc718046e54d05b208ab7ec13eb14abc342
SS3xyBEAST214 <ewilhit@hotmail.com>
2026-09-08 05:48:24 +0000

    die

---
+++ cube.py
@@ -0,0 +1,9 @@
+cublet_row=[0,0,0]
+
+cube_matrix=[
+    [0,0,0],
+    [0,0,0],
+    cublet_row
+]
+
+print(cube_matrix)
---
+++ die.py
@@ -0,0 +1,9 @@
+import random
+
+Dice=[1,2,3,4,5,6]
+
+roll=(random.choice(Dice))
+
+
+print(roll)
+
